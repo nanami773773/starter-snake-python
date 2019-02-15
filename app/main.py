b46ddba0549420ce7b417a6df917dff4b7587c8a
nanami773773 <nanamisugita@icloud.com>
2019-02-14 18:41:05 -0800

    Do i need to enter comments evrytime

--- app/main.py
+++ app/main.py
@@ -57,7 +57,8 @@ def move():
     print(json.dumps(data))
 
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    direction = up
+   " direction = random.choice(directions)"
 
     return move_response(direction)
 
